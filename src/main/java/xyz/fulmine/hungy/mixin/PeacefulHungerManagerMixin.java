0bb53cf91e179f394d27a77ccf17aecc8389e77c
Jake Fulmine <jake@fulmine.xyz>
2023-08-25 18:56:16 +0200

    hotfix for double hunger loss

--- src/main/java/xyz/fulmine/hungy/mixin/PeacefulHungerManagerMixin.java
+++ src/main/java/xyz/fulmine/hungy/mixin/PeacefulHungerManagerMixin.java
@@ -1,6 +1,8 @@
 package xyz.fulmine.hungy.mixin;
 
 import net.minecraft.entity.player.HungerManager;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.Difficulty;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -14,8 +16,8 @@ public class PeacefulHungerManagerMixin {
 			target = "Lnet/minecraft/entity/player/HungerManager;saturationLevel:F",
 			opcode = Opcodes.GETFIELD,
 			ordinal = 0), method = "update(Lnet/minecraft/entity/player/PlayerEntity;)V")
-	private void alwaysHungry(CallbackInfo info) {
-		if (((HungerManager)(Object)this).getSaturationLevel() <= 0) {
+	private void alwaysHungry(PlayerEntity player, CallbackInfo info) {
+		if (((HungerManager)(Object)this).getSaturationLevel() <= 0 && player.getWorld().getDifficulty() == Difficulty.PEACEFUL) {
 			((HungerManager)(Object)this).setFoodLevel(Math.max(((HungerManager)(Object)this).getFoodLevel() - 1, 0));
 		}
 	}
