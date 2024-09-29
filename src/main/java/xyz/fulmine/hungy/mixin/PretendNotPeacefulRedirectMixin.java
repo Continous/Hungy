f9c318a722d9caa51bfd78310d2903a23a926d75
Continous <continous159@gmail.com>
2024-09-29 03:23:29 -0700

    Update PretendNotPeacefulRedirectMixin.java

--- src/main/java/xyz/fulmine/hungy/mixin/PretendNotPeacefulRedirectMixin.java
+++ src/main/java/xyz/fulmine/hungy/mixin/PretendNotPeacefulRedirectMixin.java
@@ -6,11 +6,18 @@ import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import xyz.fulmine.hungy.config.ConfigHandler;
 
 @Mixin(PlayerEntity.class)
 public class PretendNotPeacefulRedirectMixin {
+
+    // Initialize YACL config
+    static {
+        ConfigHandler.initialize();
+    }
+
     @Redirect(method = "tickMovement()V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;getDifficulty()Lnet/minecraft/world/Difficulty;"))
     private Difficulty pretendItsNotPeaceful(World world) {
-        return Difficulty.NORMAL;
+        return ConfigHandler.getHungyDifficulty();
     }
 }
